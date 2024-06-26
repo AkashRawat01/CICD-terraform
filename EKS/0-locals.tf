1362b5cd6c9d40432631584a7207a2c489a0c781
AkashRawat01 <akash.rawat0047@gmail.com>
2024-06-26 21:28:31 +0100

    EKS setup using terraform

---
+++ EKS/0-locals.tf
@@ -0,0 +1,9 @@
+locals {
+  env         = "staging"
+  region      = "us-east-1"
+  profile     = "Akash-user"
+  zone1       = "us-east-1a"
+  zone2       = "us-east-1b"
+  eks_name    = "dev-test-1"
+  eks_version = "1.30"
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# CICD-terraform
+EKS setup using terraform
+
+### dev branch is currently in use to build and deploy EKS cluster.
