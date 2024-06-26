b8a44f59d90eecdebf0226865e07766db5e5a962
AkashRawat01 <akash.rawat0047@gmail.com>
2024-06-26 21:36:59 +0100

    EKS setup using terraform

--- EKS/0-locals.tf
+++ EKS/0-locals.tf
@@ -1,9 +1,9 @@
 locals {
-  env         = "staging"
+  env         = "Staging"
   region      = "us-east-1"
   profile     = "Akash-user"
   zone1       = "us-east-1a"
   zone2       = "us-east-1b"
-  eks_name    = "dev-test-1"
+  eks_name    = "dev-eks"
   eks_version = "1.30"
 }
